28b34df0c9ca68a653efeddba3b3b37166a4b04b
techman <dmoloney5@gmail.com>
2022-05-08 19:05:35 -0400

    had a problem with server.js and fixed it

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -6,18 +6,20 @@ const {
   createThoughts,
   updateThoughts,
   deleteThoughts,
+  addReaction,
+  deleteReaction
   
 } = require("../../controllers/thoughts-controller");
 
 router.route("/").get(getAllThoughts);
 
-router
-  .route("/:id")
-  .get(getThoughtsById)
-  .put(updateThoughts)
-  .delete(deleteThoughts);
+router.route("/:id").get(getThoughtsById).put(updateThoughts).delete(deleteThoughts);
 
-router.route("/:userId").post(createThoughts);
+router.route('/:userId').post(createThoughts);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 
 module.exports = router;
